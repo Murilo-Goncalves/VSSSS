2275fafa77e66691400ef83fc624188c21e96a52
Your Name <you@example.com>
2020-04-28 02:45:28 -0300

    started working on the simulator

---
+++ Assets/Scripts/KeyboardMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour
+{
+    public Rigidbody player;
+
+    public float force = 2000f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            player.AddForce(0, 0, force * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    
+        if (Input.GetKey("s"))
+        {
+            player.AddForce(0, 0, -force * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            player.AddForce(-force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+   
+        if (Input.GetKey("d"))
+        {
+            player.AddForce(force * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
+
